bde407823130610ac237db467c778a8b499d2460
Adam Rogowski <a.rogowski@stud.elka.pw.edu.pl>
2025-05-30 17:33:14 +0200

    Site name changed

--- languagelearningapp/settings.py
+++ languagelearningapp/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "base.apps.BaseConfig",
+    "django.contrib.sites",
 ]
+SITE_ID = 1
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
