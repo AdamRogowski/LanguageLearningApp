1ce89e40c1516856dff400de42e382bdaf373334
Adam Rogowski <a.rogowski@stud.elka.pw.edu.pl>
2025-06-18 19:13:23 +0200

    Basic PWA functionality added

---
+++ static/js/serviceworker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = "djangopwa-v2";
+const urlsToCache = [
+  "/",
+  "/static/images/logo.png",
+  "/static/images/favicon.ico",
+  "/static/styles/main.css",
+  // Add more static assets as needed
+];
+
+// Install event: cache files
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+// Activate event: clean up old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key !== CACHE_NAME)
+            .map((key) => caches.delete(key))
+        )
+      )
+  );
+  self.clients.claim();
+});
+
+// Fetch event: serve cached, then network
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(
+        (response) =>
+          response || fetch(event.request).catch(() => caches.match("/"))
+      )
+  );
+});
